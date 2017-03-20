1a2101380aedcec94ce3548e04fedb1293ec6c3a
Shaun <rocks.shaun@gmail.com>
2017-03-20 12:15:04 +0530

    App bar made more efficient

--- app/src/main/java/app/real_time_chat/MainActivity.java
+++ app/src/main/java/app/real_time_chat/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -199,5 +200,11 @@ public class MainActivity extends AppCompatActivity {
     private void onSignedOutCleanup(){
         name=NULL;
     }
-    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
